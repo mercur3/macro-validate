e194bea494356b89a02900b27c0ef95c02aa4b97
Andri Reveli <reveliandri@gmail.com>
2022-02-27 15:16:40 +0100

    Test: Constraint annotation cannot be applied on class level

--- src/test/java/com/gitlab/mercur3/macro_validate/Compiler_Test.java
+++ src/test/java/com/gitlab/mercur3/macro_validate/Compiler_Test.java
@@ -7,27 +7,50 @@ import org.junit.jupiter.api.Test;
 import static com.google.testing.compile.CompilationSubject.assertThat;
 
 class Compiler_Test {
-	private static final String simpleTestClass = """
+	private static final Compiler compiler = Compiler.javac()
+			.withProcessors(new com.gitlab.mercur3.macro_validate.Compiler());
+	private static final String simpleCorrectTestClass = """
 			package example;
-			
+
 			import com.gitlab.mercur3.macro_validate.constraints.Min;
 			import com.gitlab.mercur3.macro_validate.constraints.Valid;
-			
+
 			@Valid
-			class SimpleTestClass {
+			class SimpleCorrectTestClass {
 				@Min(value = 123, message = "Must be >= 123")
+				public int num;
+			}
+			""";
+	private static final String simpleIncorrectTestClass = """
+			package example;
+
+			import com.gitlab.mercur3.macro_validate.constraints.Min;
+			import com.gitlab.mercur3.macro_validate.constraints.Valid;
+
+			@Min(value = 123, message = "Must be >= 123")
+			class SimpleIncorrectTestClass {
 				public int num;
 			}
 			""";
 
 	@Test
 	void it_compiles() {
-		var compilation = Compiler.javac()
-				.withProcessors(new com.gitlab.mercur3.macro_validate.Compiler())
-				.compile(JavaFileObjects.forSourceString(
-						"example.SimpleTestClass",
-						simpleTestClass
-				));
+		var compilation = compiler.compile(JavaFileObjects.forSourceString(
+				"example.SimpleCorrectTestClass",
+				simpleCorrectTestClass
+		));
 		assertThat(compilation).succeededWithoutWarnings();
+	}
+
+	@Test
+	void it_does_not_compile() {
+		var compilation = compiler.compile(JavaFileObjects.forSourceString(
+				"example.SimpleIncorrectTestClass",
+				simpleIncorrectTestClass
+		));
+
+		assertThat(compilation).failed();
+		assertThat(compilation).hadErrorContaining(
+				"annotation type not applicable to this kind of declaration");
 	}
 }
